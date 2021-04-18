18876a47b7babb4b25753e7895c8ba9b6b7d288b
Maciej Gryszkiewicz <56119345+mgryszkiewicz@users.noreply.github.com>
2021-04-18 03:35:44 +0200

    Add updated version of main.py
    Add task 1, task 2, task 3 WIP

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+import hashlib
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/method")
+def get_method():
+    return {"method": "GET"}
+
+@app.put("/method")
+def put_method():
+    return {"method": "PUT"}
+
+@app.options("/method")
+def options_method():
+    return {"method": "OPTIONS"}
+
+@app.delete("/method")
+def delete_method():
+    return {"method": "DELETE"}
+
+@app.post("/method", status_code=201)
+def post_method():
+    return {"method": "POST"}
+
+@app.get("/auth{authentication}", status_code = 401)
+def get_auth(authentication, response: Response):
+    password = authentication.get("password")
+    password_hash = authentication.get("password_hash")
+    if hashlib.sha512(str(password).encode('utf-8').hexdigest()) == password_hash:
+        response.status_code = 204
+
