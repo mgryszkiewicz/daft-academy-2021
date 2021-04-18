1a697c0b8efddf1f55b0ece97fe698b6168e5c61
Maciej Gryszkiewicz <56119345+mgryszkiewicz@users.noreply.github.com>
2021-04-18 16:25:05 +0200

    Update main.py
    Add task 3, 4, 5

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, Response, status
+import datetime
 import hashlib
 
 app = FastAPI()
+app.id_counter = 0
+app.patients = dict()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!"}
 
 @app.get("/method")
 def get_method():
@@ -28,9 +31,40 @@ def post_method():
     return {"method": "POST"}
 
 @app.get("/auth", status_code = 401)
-def get_auth(password, password_hash, response: Response):
+def get_auth(password: str, password_hash: str, response: Response):
     # password = authentication.get("password")
     # password_hash = authentication.get("password_hash")
     if hashlib.sha512(str(password).encode('utf-8')).hexdigest() == password_hash:
         response.status_code = 204
 
+@app.post("/register", status_code=201)
+def post_register(data: dict, response: Response):
+    name = data.get("name")
+    surname = data.get("surname")
+
+    if name is None or surname is None:
+        response.status_code = 422
+        return
+
+    app.id_counter += 1
+    app.patients[app.id_counter] = {
+            "id": app.id_counter,
+            "name": name,
+            "surname": surname,
+            "register_date": (datetime.datetime.today()).strftime('%Y-%m-%d'),
+            "vaccination_date": (datetime.datetime.today() + datetime.timedelta(days=len(name) + len(surname))).strftime('%Y-%m-%d')
+            }
+    return app.patients[app.id_counter]
+
+
+@app.get("/patient/{patient_id}", status_code=200)
+def get_patient(patient_id: int, response: Response):
+    print("\n\n\n\nHaloooo\n\n\n" + str(app.patients))
+    if patient_id < 1:
+        response.status_code = 400
+        return
+    elif patient_id not in app.patients:
+        response.status_code = 404
+        return
+    else:
+        return app.patients[patient_id]
